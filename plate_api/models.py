2f3a0e9bd2f9d4a789669bef81ec10cce1ebf972
Kaveh Gholizadeh <Sobhan.Gholizadeh.K@gmail.com>
2024-12-10 20:03:55 +0330

    fixed image url in models.py

--- plate_api/models.py
+++ plate_api/models.py
@@ -6,7 +6,7 @@ class Vehicle(models.Model):
     # Character field with a maximum length of 255 characters for vehicle plate number
     vehicle_plate = models.CharField(max_length=255, null=False, unique=True)
     status = models.BooleanField(default=False)
-    image_url = models.URLField()
+    image_url = models.URLField(blank=True, null=True, default='')
 
     def __str__(self):  # Human-readable string representation
         return self.vehicle_plate
